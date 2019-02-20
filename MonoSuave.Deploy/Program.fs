7fe114b58997bb242e87af6bd63a165277aba5c3
bdimpe@gmail <imaginarydevelopment@gmail.com>
2019-02-20 14:32:38 -0500

    add some prototype rest deploy code

--- MonoSuave.Deploy/Program.fs
+++ MonoSuave.Deploy/Program.fs
@@ -1,19 +1,62 @@
 ﻿open System
 open System.Diagnostics
 open System.IO
+open System.Net.Http
+open System.Text
 
 // Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
-let appveyorRest() =
+// if we somehow find a way to run this on the azure command line
+let locateZip () =
     let zipPath = "MonoSuave\bin\Release"
     if not <| Directory.Exists zipPath then
         let fullPath = Path.GetFullPath zipPath
         eprintfn "zip not found at %s" fullPath
-        1
+        None
     else
         let zipFilePath = Path.Combine(zipPath,"Release.zip")
-        System.IO.Compression.ZipFile.ExtractToDirectory(zipFilePath,"unzipped")
-        0
+        Some zipFilePath
+        //System.IO.Compression.ZipFile.ExtractToDirectory(zipFilePath,"unzipped")
+let (|ValueString|NonValueString|) =
+    function
+    | null | "" -> NonValueString
+    | x when String.IsNullOrWhiteSpace x -> NonValueString
+    | x -> ValueString x
+
+let appveyorRest() =
+    let expand x = Environment.ExpandEnvironmentVariables x
+    match expand "%restuser%", expand "%restpwd%" with
+    | ValueString u, ValueString p ->
+        locateZip()
+        |> Option.map(fun zp ->
+            async{
+                let bytes = File.ReadAllBytes(zp)
+                use form = new MultipartFormDataContent()
+                use bac = new ByteArrayContent(bytes,0,bytes.Length)
+                form.Add(bac,"myUpdatePackage",Path.GetFileName(zp))
+
+                use wc = new HttpClient()
+                let encodeAuth u p =
+                    sprintf "%s:%s" u p
+                    |> Encoding.ASCII.GetBytes
+                    |> Convert.ToBase64String
+                wc.DefaultRequestHeaders.Authorization <- System.Net.Http.Headers.AuthenticationHeaderValue("Basic", encodeAuth u p)
+                let! r = Async.AwaitTask <| wc.PostAsync("https://imaginarysuave.scm.azurewebsites.net/api/zipdeploy",form)
+                return if r.IsSuccessStatusCode then 0 else 1
+            }
+            |> Async.RunSynchronously
+        )
+        |> Option.defaultValue 2
+    | NonValueString, NonValueString ->
+        eprintfn "Rest: no username or password found"
+        3
+    | ValueString _, _ ->
+        eprintfn "Rest: Username found, but no password"
+        4
+    | NonValueString, ValueString _ ->
+        eprintfn "Rest: No Username found, but found password"
+        5
+
 [<EntryPoint>]
 let main argv = 
     printfn "MonoSuave.Deploy running"
